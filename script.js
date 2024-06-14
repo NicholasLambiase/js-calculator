d936866f1194c255e52c397473adeff1c8116beb
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-06-14 12:05:46 -0700

    Got the number keys to be responsive Made the display append the numbers that are pressed

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let firstOperand;
-let operator;
-let secondOperand;
+let firstOperand = "";
+let operator = "";
+let operator_selected = false;
+let secondOperand = "";
+
+let display_cnt = 0;
+let result;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
 
 function operate(firstOperand, operator, secondOperand) {
-    let result
     switch (operator) {
         case "+":
             result = add(firstOperand, secondOperand);
@@ -24,27 +43,30 @@ function operate(firstOperand, operator, secondOperand) {
 }
 
 
-function add(a, b) {
-    return a + b;
-}
+let num_btns = document.querySelectorAll(".num-btn");
+num_btns.forEach(num => {
+    num.addEventListener("click", () => {
+        if (display_cnt < 8) {
+            let entered_num = num.textContent;
+            
+            firstOperand = firstOperand + entered_num;
+            
+            let display_num = document.createElement("p");
+            display_num.classList = "result-txt"
+            display_num.textContent = entered_num;
+            
+            let display_area = document.querySelector("#display-area");
+            display_area.appendChild(display_num);
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
+            display_cnt += 1;
+        }
+    });
+});
 
 // Switch Easter Egg Functionality
 const switchToggle = document.getElementById('switch-toggle');
 
 switchToggle.addEventListener('change', function() {
   const isChecked = this.checked;
-  // Perform any desired actions based on the switch state
   console.log(`Switch is ${isChecked ? 'on' : 'off'}`);
 });
