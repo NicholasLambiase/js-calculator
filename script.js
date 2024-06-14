05828c1cafccaa34fb3eaa48f946b8dcd22f1111
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-06-14 12:51:21 -0700

    Added the Clear and Clear Entry Functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let operator = "";
 let operator_selected = false;
 let secondOperand = "";
 
+let display_area = document.querySelector("#display-area");
 let display_cnt = 0;
 let result;
 
@@ -42,30 +43,77 @@ function operate(firstOperand, operator, secondOperand) {
     }
 }
 
+function clearDisplay() {
+    let display_nums = document.querySelectorAll(".result-txt");
+    if (display_nums){
+        display_nums.forEach(num => {
+            num.remove();
+        });
+    }
+    display_cnt = 0;
+}
 
-let num_btns = document.querySelectorAll(".num-btn");
+const num_btns = document.querySelectorAll(".num-btn");
 num_btns.forEach(num => {
     num.addEventListener("click", () => {
         if (display_cnt < 8) {
             let entered_num = num.textContent;
             
-            firstOperand = firstOperand + entered_num;
+            if (!operator_selected)
+                firstOperand = firstOperand + entered_num;
+            else
+                secondOperand = secondOperand + entered_num;
             
             let display_num = document.createElement("p");
             display_num.classList = "result-txt"
             display_num.textContent = entered_num;
             
-            let display_area = document.querySelector("#display-area");
-            display_area.appendChild(display_num);
+            display_area.prepend(display_num);
 
             display_cnt += 1;
         }
     });
 });
 
-// Switch Easter Egg Functionality
-const switchToggle = document.getElementById('switch-toggle');
 
+// Operator btns
+// When clicked need to clear the screen, set the operand
+
+
+// Equals btn
+// When clicked should either calculate or throw an error
+// If calculated set this to the display and set firstOperand = result
+
+
+// Clear Entry Button
+// If the operator boolean is false clear the first operand
+// else clear the second operand
+// Regardless: clear the display
+const clear_entry_btn = document.querySelector("#clear-entry");
+clear_entry_btn.addEventListener("click", () => {
+    clearDisplay();
+
+    if (!operator_selected)
+        firstOperand = "";
+    else
+        secondOperand = "";
+});
+
+
+// Clear Button
+// Clear the first and second operand. Reset operator boolean
+// Regardless: clear the display
+const clear_btn = document.querySelector("#clear-all");
+clear_btn.addEventListener("click", () => {
+    clearDisplay();
+
+    firstOperand = "";
+    secondOperand = "";
+    operator = "";
+    operator_selected = false;
+});
+
+const switchToggle = document.getElementById('switch-toggle');
 switchToggle.addEventListener('change', function() {
   const isChecked = this.checked;
   console.log(`Switch is ${isChecked ? 'on' : 'off'}`);
